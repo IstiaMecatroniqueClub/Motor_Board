138dbe5f88c09349748d6c58dd4b5d79a6832f20
Franck Mercier <f.mercier@ad.univ-angers.fr>
2017-09-18 09:40:36 +0200

    Ajout lib CanISR, simplification du code, fonctionnement PID on par defaut

---
+++ code/include/CanISR.h
@@ -0,0 +1,153 @@
+/*
+ * CPPFile1.cpp
+ *
+ * Created: 24/05/2017 12:28:09
+ *  Author: f.mercier
+ */ 
+
+
+ //! \fn void initCANBus()
+ //! \brief Initialize the CAN Bus.
+ //!
+ //! Function that initializes the CANBus according to the wiring and the needed speed (500kb/s).
+ //! It also enable the CAN module interruptions
+ void initCANBus () 
+ {
+	 // Enable of CAN transmit
+	 DDRC |= 0x80; // Configuration of the standby pin as an output
+	 // (for a transceiver MCP2562 with the stby pin wired to the uC PC7)
+	 // Enable the MCP2562 (STANDBY to 0)
+	 PORTC &= 0x7F; // Activation of the MCP2562
+	 // (for a transceiver MCP2562 with the stby pin wired to the uC PC7)
+
+	 // Initialization of CAN module
+	 CANGCON  = (1 << SWRES); // Reset CAN module
+	 CANGCON  = 0x02; // Enable CAN module
+
+	 CANBT1 = 0x06; // Speed Config (500kb/s)
+	 CANBT2 = 0x04;
+	 CANBT3 = 0x13;
+
+	 CANHPMOB = 0x00; // No priority between the CAN MOB
+
+	 // Enable CAN interruptions and especially reception interruptions
+	 CANGIE |= 0xA0;
+ }
+
+ //! \fn void initCANMOBasReceiver()
+ //! \brief Initialize the CAN MOB.
+ //!
+ //! Initialize a CAN MOB between 0 to 6 as a receiver. A MOB can be seen as a "CAN socket".
+ void initCANMOBasReceiver (uint8_t mobNumber, uint32_t ID, uint8_t rtr) 
+ {
+	 CANPAGE = (mobNumber << 4) & 0xF0;
+	 //CANPAGE = 0x10; // selection of MOB1 (reveice request)
+
+	 if (rtr) { CANIDT4 = 0x04;}
+	 else { CANIDT4 = 0x00;}
+	 //CANIDT4 = 0x04; // Config as reception remote (rtr = 1)
+	 CANIDT3 = 0x00;
+	 CANIDT2 = (uint8_t)( (ID & 0x00F)<< 5 ); // using the constant to configure the remote request identifier
+	 CANIDT1 = (uint8_t)( ID >> 3 );
+
+	 if (rtr) { CANIDM4 = 0x00;}
+	 else { CANIDM4 = 0x04;}
+	 //CANIDM4 = 0x04; // mask over the rtr value
+	 CANIDM3 = 0xFF; // mask over the identifier (we only want an interruption if the received CAN message is a remote request with the corresponding identifier)
+	 CANIDM2 = 0xFF;
+	 CANIDM1 = 0xFF;
+
+	 CANCDMOB = 0x80; // Config MOB as reception
+	 
+	 switch (mobNumber) {
+		 case 0 : CANIE2 |= 0x01; break;
+		 case 1 : CANIE2 |= 0x02; break;
+		 case 2 : CANIE2 |= 0x04; break;
+		 case 3 : CANIE2 |= 0x08; break;
+		 case 4 : CANIE2 |= 0x10; break;
+		 case 5 : CANIE2 |= 0x20; break;
+		 default : break;
+	 }
+	 //CANIE2 = 0x02; // enable the interruptions over the MOB 1
+
+	 sei(); // set enable interruption
+ }
+
+ //! \fn void initCANMOBasIDBandReceiver()
+ //! \brief Initialize the CAN MOB.
+ //!
+ //! Initialize a CAN MOB between 0 to 6 as ID band receiver. A MOB can be seen as a "CAN socket" on different successive ID.
+ void initCANMOBasIDBandReceiver (uint8_t mobNumber, uint32_t BeginingID, uint32_t AreaSize, uint8_t rtr)
+ {
+	 CANPAGE = (mobNumber << 4) & 0xF0;
+
+	 if (rtr) { CANIDT4 = 0x04;}
+	 else { CANIDT4 = 0x00;}
+	 CANIDT3 = 0x00;
+	 CANIDT2 = (uint8_t)( (BeginingID & 0x000F)<< 5 ); // using the constant to configure the remote request identifier
+	 CANIDT1 = (uint8_t)( BeginingID >> 3 );
+
+	 AreaSize = 0x00000000 - AreaSize;
+	 if (rtr) { CANIDM4 = 0x00;}
+	 else { CANIDM4 = 0x04;}
+	 CANIDM3 = 0xFF; // mask over the identifier (we only want an interruption if the received CAN message is a remote request with the corresponding identifier)
+	 CANIDM2 = (uint8_t)( (AreaSize & 0x000F)<< 5 );
+	 CANIDM1 = (uint8_t)( AreaSize >> 3 );
+
+	 CANCDMOB = 0x80; // Config MOB as reception
+	 
+	 switch (mobNumber) {
+		 case 0 : CANIE2 |= 0x01; break;
+		 case 1 : CANIE2 |= 0x02; break;
+		 case 2 : CANIE2 |= 0x04; break;
+		 case 3 : CANIE2 |= 0x08; break;
+		 case 4 : CANIE2 |= 0x10; break;
+		 case 5 : CANIE2 |= 0x20; break;
+		 default : break;
+	 }
+
+	 sei(); // set enable interruption
+ }
+
+  //! \fn void disableCANMOB()
+  //! \brief Resetting the CAN MOB.
+  //!
+  //! Reset a CAN MOB between 0 to 6.
+  void disableCANMOB (uint8_t mobNumber)
+  {
+	  CANPAGE = (mobNumber << 4) & 0xF0;			// selection of correct MOB
+
+	  CANCDMOB = 0x00;								// Config MOB as disable
+	  
+	  switch (mobNumber) {							// Disable the interruptions over the proper MOB
+		  case 0 : CANIE2 &= 0xFE; break;
+		  case 1 : CANIE2 &= 0xFD; break;
+		  case 2 : CANIE2 &= 0xFB; break;
+		  case 3 : CANIE2 &= 0xF7; break;
+		  case 4 : CANIE2 &= 0xEF; break;
+		  case 5 : CANIE2 &= 0xDF; break;
+		  default : break;
+	  }
+  }
+
+  //! \fn void sendData()
+  //! \brief Sending data over a CAN MOB.
+  //!
+  //! Sending data through a CAN MOB between 0 to 6.
+  void sendData (uint8_t mobNumber, uint32_t ID, uint8_t dlc, uint8_t* buffer)
+  {
+	CANPAGE = (mobNumber << 4) & 0xF0;				// Mob selection
+
+	CANIDT4 = 0x00;									// Config as data (rtr = 0)
+	CANIDT3 = 0x00;
+	CANIDT2 = (uint8_t)( (ID & 0x00F)<< 5 );		// identifier implementation
+	CANIDT1 = (uint8_t)( ID >> 3 );
+
+	for (int i=0; i < dlc; i++)
+	{
+		CANPAGE &= 0xF0;
+		CANPAGE |= i;
+		CANMSG = buffer[i];
+	}
+	CANCDMOB = 0x40 | dlc;							// send the message using the proper MOB
+  }
--- code/main.cpp
+++ code/main.cpp
@@ -22,6 +22,7 @@
 #include "spi.h"
 #include "counter.h"
 #include "pid.h"
+#include "CanISR.h"
 
 #include <string.h> //POUR LES TESTS
 
@@ -30,7 +31,7 @@
 #define RIGHT_MOTOR             (1)         //!< The right motor value
 #define LEFT_MOTOR              (-1)        //!< The left motor value
 
-#define SIDE_MOTOR              RIGHT_MOTOR  //!< To handle left and right moteur
+#define SIDE_MOTOR              LEFT_MOTOR  //!< To handle left and right moteur
                                             //!  choose between LEFT_MOTOR or RIGHT_MOTOR
 
 #define LED_RED_PORT            PORTB       //!< The port for the red LED
@@ -41,9 +42,9 @@
 #define LED_YELLOW_PIN          2           //!< The pin for the yellow LED
 #define LED_YELLOW_POL          0           //!< The polarity of the yellow LED
 
-#define ID_MOTORBOARD_DATASPEED 0x35        //!< The polarity of the yellow LED
-#define ID_MOTORBOARD_DATAPID   0x37        //!< The polarity of the yellow LED
-#define ID_MOTORBOARD_DATACONF  0x36        //!< The polarity of the yellow LED
+#define ID_MOTORBOARD_DATASPEED 0x030       //!< The polarity of the yellow LED
+#define ID_MOTORBOARD_DATACONF  0x031       //!< The polarity of the yellow LED
+#define ID_MOTORBOARD_DATAPID   0x032       //!< The polarity of the yellow LED
 
 #define NB_STEPS                1920        //!< Number of tics for a complete wheel turn
 
@@ -53,121 +54,12 @@
                                             //!  shutting down the robot (avoid motion after
                                             //!  an emmergency stop for instance)
 
-#define DEFAULT_KP              0.1         //!< default KP for the PID 
-#define DEFAULT_KI              0           //!< default KI for the PID
-#define DEFAULT_KD              0           //!< default KD for the PID
+#define DEFAULT_KP              0.05         //!< default KP for the PID 
+#define DEFAULT_KI              0.0009           //!< default KI for the PID
+#define DEFAULT_KD              0.8           //!< default KD for the PID
 
 #define F_MOTOR_TIC2PWM(tic) (SIDE_MOTOR*35*tic) //!< To convert counter value to PWM,
                                                  //!  the values are extracted from experimental tests
-
-//! \fn void initCANBus()
-//! \brief Intialize the CAN Bus.
-//!
-//! Function that initializes the CANBus according to the wiring and the needed speed (500kb/s).
-//! It also enable the CAN module interruptions
-void initCANBus(){
-    // Enable of CAN transmit
-    DDRC|=0x80; // Configuration of the standby pin as an output
-                // (for a transceiver MCP2562 with the stby pin wired to the uC PC7)
-    // Enable the MCP2562 (STANDBY to 0)
-    PORTC &= 0x7F; // Activation of the MCP2562
-                   // (for a transceiver MCP2562 with the stby pin wired to the uC PC7)
-
-    // Initialization of CAN module
-    CANGCON  = (1 << SWRES); // Reset CAN module
-    CANGCON  = 0x02; // Enable CAN module
-
-    CANBT1 = 0x06; // Speed Config (500kb/s)
-    CANBT2 = 0x04;
-    CANBT3 = 0x13;
-
-    CANHPMOB = 0x00; // No priority between the CAN MOB
-
-    // Enable CAN interruptions and especially reception interruptions
-    CANGIE |= 0xA0;
-}
-
-// POUR LES TESTS
-void sendCANdata(uint8_t id, uint8_t dlc, uint8_t* data){
-    CANPAGE  = 0x00; //Selection of MOB 0
-
-    CANIDT4 = 0x00; // Config as data (rtr = 0)
-    CANIDT3 = 0x00;
-    CANIDT2 = (uint8_t)( (id & 0x00F)<< 5 ); // set the identifier
-    CANIDT1 = (uint8_t)( id >> 3 );
-
-    for(uint8_t i=0; i< dlc; i++){
-        CANMSG = data[dlc-1-i];
-        CANPAGE = 0x00 + 1 + i;
-    }
-
-    CANCDMOB = 0x40 + (0x0F & dlc);// send the message using the MOB 0
-
-    while ( !(CANSTMOB & (1 << TXOK)));
-}
-
-
-//! \fn void initCANMOB()
-//! \brief Intialize the CAN MOB.
-//!
-//! Intialize the CAN MOB. A MOB can be seen as a "CAN socket".
-//! In this case the MOB1 is used to receive data request (speed value)
-//! In this case the MOB2 is used to receive data request (pid value)
-void initCANMOB(){
-    //--- MOB 1 : set motor speed (receive data) ---
-    CANPAGE = 0x10; // selection of MOB1 (receive data speed)
-
-    CANIDT4 = 0x00; // Config as reception data (rtr = 0)
-    CANIDT3 = 0x00;
-    CANIDT2 = (uint8_t)( (ID_MOTORBOARD_DATASPEED & 0x00F)<< 5 ); // using the constant to configure the remote request identifier
-    CANIDT1 = (uint8_t)( ID_MOTORBOARD_DATASPEED >> 3 );
-
-    CANIDM4 = 0x04; // mask over the rtr value
-    CANIDM3 = 0xFF; // mask over the identifier (we only want an interruption if the received CAN message is a remote request with the corresponding identifier)
-    CANIDM2 = 0xFF;
-    CANIDM1 = 0xFF;
-
-    CANCDMOB = 0x80; // Config MOB as reception
-
-    //--- MOB 2 : set PID value (receive data) ---
-    CANPAGE = 0x20; // selection of MOB2 (receive PID data)
-
-    CANIDT4 = 0x00; // Config as reception data (rtr = 0)
-    CANIDT3 = 0x00;
-    CANIDT2 = (uint8_t)( (ID_MOTORBOARD_DATAPID & 0x00F)<< 5 ); // using the constant to configure the remote request identifier
-    CANIDT1 = (uint8_t)( ID_MOTORBOARD_DATAPID >> 3 );
-
-    CANIDM4 = 0x04; // mask over the rtr value
-    CANIDM3 = 0xFF; // mask over the identifier (we only want an interruption if the received CAN message is a remote request with the corresponding identifier)
-    CANIDM2 = 0xFF;
-    CANIDM1 = 0xFF;
-
-    CANCDMOB = 0x80; // Config MOB as reception
-
-    //--- MOB 3 : CONFIGURATION - enable PID - (receive data) ---
-    CANPAGE = 0x30; // selection of MOB3 (receive Configuration word)
-
-    CANIDT4 = 0x00; // Config as reception data (rtr = 0)
-    CANIDT3 = 0x00;
-    CANIDT2 = (uint8_t)( (ID_MOTORBOARD_DATACONF & 0x00F)<< 5 ); // using the constant to configure the remote request identifier
-    CANIDT1 = (uint8_t)( ID_MOTORBOARD_DATACONF >> 3 );
-
-    CANIDM4 = 0x04; // mask over the rtr value
-    CANIDM3 = 0xFF; // mask over the identifier (we only want an interruption if the received CAN message is a remote request with the corresponding identifier)
-    CANIDM2 = 0xFF;
-    CANIDM1 = 0xFF;
-
-    CANCDMOB = 0x80; // Config MOB as reception
-    
-    CANIE2 = 0x0E; // enable the interruptions over the MOB 1, MOB2 and MOB3 (0b 0000 1110)
-}
-
-// POUR LES TESTS
-void printCAN(void* variable, size_t size, uint8_t id=0xFF){
-    uint8_t data[8];
-    memcpy(data, variable, size);
-    sendCANdata(id, size, data);
-}
 
 Led redLed(&LED_RED_PORT, LED_RED_PIN, LED_RED_POL);             //!< the red LED
 Led yellowLed(&LED_YELLOW_PORT, LED_YELLOW_PIN, LED_YELLOW_POL); //!< the yellow LED
@@ -192,7 +84,7 @@ int main(void)
 
     // initialization of the flags and other global variables
     watch_dog = 0;
-    enablePID = 1;
+    enablePID = 0;
     nb_tics_cmd = 0;
     nb_tics_target = 0;
     nbFlat = 0;
@@ -211,7 +103,7 @@ int main(void)
     TIFR1  |= 0x04;
     TCCR1B |= 0x0D;
     TCCR1A |= 0;
-    OCR1A   = 78;
+    OCR1A   = 390;
     // Value for the interruption (OCR1A)
     // 7812 = 1   sec
     // 1562 = 200 ms
@@ -229,8 +121,8 @@ int main(void)
     counter.clear_counter(); // reset the counter value
     counter.clear_status_register(); // clear the counter register
 
-    initCANBus(); // intialization of the CAN Bus
-    initCANMOB(); // intialization of the CAN MOB
+    initCANBus(); // initialization of the CAN Bus
+    initCANMOBasReceiver (1, ID_MOTORBOARD_DATASPEED, 0); // initialization of the CAN MOB
 
     motor.enableMotor(); // enable the motor
 
@@ -260,20 +152,20 @@ ISR(TIMER1_COMPA_vect){
         nbFlat = 0; // reset the flat flag
     }
 
-    if(watch_dog > MAX_WATCH_DOG || nb_tics_target == 0 || nbFlat > MAX_NB_FLAT){ 
+    if(watch_dog > MAX_WATCH_DOG || nb_tics_target == 0 || nbFlat > MAX_NB_FLAT){	//  || nb_tics_target == 0
         // the motor is stopped if:
         //      - the time of the received last command is over the watch dog delay
         //      - the speed command is 0
-        //      - the number of 0 counter value is over the max value (possible emmergency stop)
+        //      - the number of 0 counter value is over the max value (possible emergency stop)
+        if (enablePID) {pid.reset(); } // reset the PID
         motor.setSpeed(0);
-        pid.reset(); // reset the PID
         nb_tics_target = 0; // reset the speed target
     }else{
         watch_dog ++; // increments the watch dog (reseted when receiving new speed command)
-        if(enablePID==0x01){ // if the PID is enabled
+        if(enablePID){ // if the PID is enabled
             // compute the corrected command with the PID
             int16_t cmd = nb_tics_cmd + pid.update(nb_tics_target, val);
-            // set the motor speed 
+            // set the motor speed
             motor.setSpeed(F_MOTOR_TIC2PWM(cmd));
         }else{
             // if the PID is desactivated, set directly the motor with the estimated transfer function
@@ -291,7 +183,7 @@ ISR(CAN_INT_vect){
 
     if ( (CANSIT2 & 0x02 )!=0x00){ // MOB1 interruption - SET MOTOR SPEED
         CANPAGE = 0x10; // Selection of MOB 1
-
+		
         // get the data from the mob 1:
         uint8_t dlc = CANCDMOB & 0x0F; // get the DLC of the CAN frame
         if(dlc == 0x03){ // rotationCW | speed(MSB) | speed(LSB)
@@ -302,10 +194,12 @@ ISR(CAN_INT_vect){
             CANPAGE = 0x11; 
             uint8_t speedH = CANMSG;
             // get the third and last data byte (speed LSB)
-            CANPAGE = 0x12; 
+            CANPAGE = 0x12;
             uint8_t speedL = CANMSG;
             // get the target speed (integer mrad/s)
             uint16_t mrads = (uint16_t)(speedH << 8) + speedL;
+			//uint16_t mrads = 0x8000;
+			//rotationCW = 01;
             int16_t nb_tics_new_target = 0; // to update the speed target (counter value)
             watch_dog = 0; // reset the watch dog (a new command has been received)
             if(mrads == 0){ // if the speed is 0, special case
@@ -320,20 +214,19 @@ ISR(CAN_INT_vect){
                 // if the target speed has been changed
                 nb_tics_target = nb_tics_new_target; // update the target
                 nb_tics_cmd = nb_tics_target; // update the command according to the target
-                pid.reset(); // reset the PID
+                if (enablePID) {pid.reset(); } // reset the PID
                 nbFlat = 0; // reset the nbFlat flag
             } // otherwise, nothing to change
         }
-        // else: the data is not correct
 
         // reset the MOB1 configuration for next CAN message
         CANPAGE   = 0x10; // select MOB1
         CANSTMOB  = 0x00; // Reset the status of the MOB1
         CANCDMOB  = 0x80; // Config as reception MOB1
-        CANIE2   |= 0x02; // Enable the interuption over MOB 1 (for the next one)
+        CANIE2   |= 0x02; // Enable the interruption over MOB 1 (for the next one)
         CANSIT2  &= 0xFD; // remove the MOB1 raised flag
 
-    }else if((CANSIT2 & 0x04 )!=0x00){ //MOB2 interruption - Set PID coefficients
+    }/*else if((CANSIT2 & 0x04 )!=0x00){ //MOB2 interruption - Set PID coefficients
         // 1 -> set KP
         // 2 -> set KI
         // 3 -> set KD
@@ -400,12 +293,13 @@ ISR(CAN_INT_vect){
         // else: the data is not correct
 
         // reset the MOB3 configuration for next CAN message
-        CANPAGE   = 0x30; // select MOB3
-        CANSTMOB  = 0x00; // Reset the status of the MOB3
-        CANCDMOB  = 0x80; // Config as reception MOB3
-        CANIE2   |= 0x08; // Enable the interuption over MOB 3 (for the next one)
-        CANSIT2  &= 0xF7; // remove the MOB3 raised flag
-    }
+		initCANMOBasReceiver(3, ID_MOTORBOARD_DATACONF, 0);
+        //CANPAGE   = 0x30; // select MOB3
+        //CANSTMOB  = 0x00; // Reset the status of the MOB3
+        //CANCDMOB  = 0x80; // Config as reception MOB3
+        //CANIE2   |= 0x08; // Enable the interruption over MOB 3 (for the next one)
+        //CANSIT2  &= 0xF7; // remove the MOB3 raised flag
+    }*/
 
     sei(); // enable the interruptions
 }
